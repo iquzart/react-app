6695aa3b21b77074dacb6bed94b8f44138d77707
Muhammed Iqbal <iquzart@hotmail.com>
2020-09-14 14:07:15 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Herald Web Application </h1>
+        <h1> Demo - React Web Application </h1>
       </header>
     </div>
   );
